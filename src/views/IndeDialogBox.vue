6b34ec63ba8bcec186cd2d445434b92511342e5e
yuko0931 <bxj0931@gmail.com>
2025-02-02 18:23:00 +0800

    feat：组件使用&样式修改

--- src/views/IndeDialogBox.vue
+++ src/views/IndeDialogBox.vue
@@ -3,67 +3,76 @@
     <div class="side">历史会话</div>
     <div class="main-wrapper">
       <div class="header">this is header</div>
-      <div class="content">this is content area(when query exists</div>
-      <div class="input-box">
-        <div class="tip">有什么可以帮忙的？</div>
-        <div class="input-wrapper">
-          <div class="textarea-wrapper">
-            <textarea
-              v-model="inputMessage"
-              name="input-area"
-              placeholder="请输入要询问的问题"
-              id="input-area"
-            ></textarea>
-          </div>
-          <div class="button-wrapper">
-            <div class="attach btn" @click="handleUploadAttach">
-              <input
-                ref="fileInput"
-                type="file"
-                multiple
-                accept=".pdf,.png,.jpg,.jpeg,.svg,.svgz,.bmp,.gif,.webp,.ico,.xbm,.dib,.doc,.docx,.ppt,.pptx,.xls,.xlsx,.txt,.md"
-                style="display: none"
-              />
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 20" fill="none">
-                <path
-                  d="M7 20c-1.856-.002-3.635-.7-4.947-1.94C.74 16.819.003 15.137 0 13.383V4.828a4.536 4.536 0 0 1 .365-1.843 4.75 4.75 0 0 1 1.087-1.567A5.065 5.065 0 0 1 3.096.368a5.293 5.293 0 0 1 3.888 0c.616.244 1.174.6 1.643 1.05.469.45.839.982 1.088 1.567.25.586.373 1.212.364 1.843v8.555a2.837 2.837 0 0 1-.92 2.027A3.174 3.174 0 0 1 7 16.245c-.807 0-1.582-.3-2.158-.835a2.837 2.837 0 0 1-.92-2.027v-6.22a1.119 1.119 0 1 1 2.237 0v6.22a.777.777 0 0 0 .256.547.868.868 0 0 0 .585.224c.219 0 .429-.08.586-.224a.777.777 0 0 0 .256-.546V4.828A2.522 2.522 0 0 0 7.643 3.8a2.64 2.64 0 0 0-.604-.876 2.816 2.816 0 0 0-.915-.587 2.943 2.943 0 0 0-2.168 0 2.816 2.816 0 0 0-.916.587 2.64 2.64 0 0 0-.604.876 2.522 2.522 0 0 0-.198 1.028v8.555c0 1.194.501 2.339 1.394 3.183A4.906 4.906 0 0 0 7 17.885a4.906 4.906 0 0 0 3.367-1.319 4.382 4.382 0 0 0 1.395-3.183v-6.22a1.119 1.119 0 0 1 2.237 0v6.22c-.002 1.754-.74 3.436-2.052 4.677C10.635 19.3 8.856 19.998 7 20z"
-                  fill="currentColor"
-                ></path>
-              </svg>
+      <div class="chat-wrapper">
+        <div class="chat-list" v-if="!showTip">
+          <div v-for="(message, index) in messages" :key="index">
+            <div>
+              <MyQuestion :message="message"></MyQuestion>
             </div>
-            <div
-              class="send btn"
-              :class="inputMessage.trim() ? 'active' : ''"
-              @click="handleSendMessage"
-            >
-              <svg
-                width="14"
-                height="16"
-                viewBox="0 0 14 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
+          </div>
+        </div>
+        <div class="input-box" :class="{ fixed: isFixed }">
+          <div class="tip" v-if="showTip">有什么可以帮忙的？</div>
+          <div class="input-wrapper">
+            <div class="textarea-wrapper">
+              <textarea
+                v-model="inputMessage"
+                name="input-area"
+                placeholder="请输入要询问的问题"
+                id="input-area"
+              ></textarea>
+            </div>
+            <div class="button-wrapper">
+              <div class="attach btn" @click="handleUploadAttach">
+                <input
+                  ref="fileInput"
+                  type="file"
+                  multiple
+                  accept=".pdf,.png,.jpg,.jpeg,.svg,.svgz,.bmp,.gif,.webp,.ico,.xbm,.dib,.doc,.docx,.ppt,.pptx,.xls,.xlsx,.txt,.md"
+                  style="display: none"
+                />
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 20" fill="none">
+                  <path
+                    d="M7 20c-1.856-.002-3.635-.7-4.947-1.94C.74 16.819.003 15.137 0 13.383V4.828a4.536 4.536 0 0 1 .365-1.843 4.75 4.75 0 0 1 1.087-1.567A5.065 5.065 0 0 1 3.096.368a5.293 5.293 0 0 1 3.888 0c.616.244 1.174.6 1.643 1.05.469.45.839.982 1.088 1.567.25.586.373 1.212.364 1.843v8.555a2.837 2.837 0 0 1-.92 2.027A3.174 3.174 0 0 1 7 16.245c-.807 0-1.582-.3-2.158-.835a2.837 2.837 0 0 1-.92-2.027v-6.22a1.119 1.119 0 1 1 2.237 0v6.22a.777.777 0 0 0 .256.547.868.868 0 0 0 .585.224c.219 0 .429-.08.586-.224a.777.777 0 0 0 .256-.546V4.828A2.522 2.522 0 0 0 7.643 3.8a2.64 2.64 0 0 0-.604-.876 2.816 2.816 0 0 0-.915-.587 2.943 2.943 0 0 0-2.168 0 2.816 2.816 0 0 0-.916.587 2.64 2.64 0 0 0-.604.876 2.522 2.522 0 0 0-.198 1.028v8.555c0 1.194.501 2.339 1.394 3.183A4.906 4.906 0 0 0 7 17.885a4.906 4.906 0 0 0 3.367-1.319 4.382 4.382 0 0 0 1.395-3.183v-6.22a1.119 1.119 0 0 1 2.237 0v6.22c-.002 1.754-.74 3.436-2.052 4.677C10.635 19.3 8.856 19.998 7 20z"
+                    fill="currentColor"
+                  ></path>
+                </svg>
+              </div>
+              <div
+                class="send btn"
+                :class="inputMessage.trim() ? 'active' : ''"
+                @click="handleSendMessage"
               >
-                <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M7 16c-.595 0-1.077-.462-1.077-1.032V1.032C5.923.462 6.405 0 7 0s1.077.462 1.077 1.032v13.936C8.077 15.538 7.595 16 7 16z"
-                  fill="currentColor"
-                ></path>
-                <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M.315 7.44a1.002 1.002 0 0 1 0-1.46L6.238.302a1.11 1.11 0 0 1 1.523 0c.421.403.421 1.057 0 1.46L1.838 7.44a1.11 1.11 0 0 1-1.523 0z"
-                  fill="currentColor"
-                ></path>
-                <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M13.685 7.44a1.11 1.11 0 0 1-1.523 0L6.238 1.762a1.002 1.002 0 0 1 0-1.46 1.11 1.11 0 0 1 1.523 0l5.924 5.678c.42.403.42 1.056 0 1.46z"
-                  fill="currentColor"
-                ></path>
-              </svg>
+                <svg
+                  width="14"
+                  height="16"
+                  viewBox="0 0 14 16"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M7 16c-.595 0-1.077-.462-1.077-1.032V1.032C5.923.462 6.405 0 7 0s1.077.462 1.077 1.032v13.936C8.077 15.538 7.595 16 7 16z"
+                    fill="currentColor"
+                  ></path>
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M.315 7.44a1.002 1.002 0 0 1 0-1.46L6.238.302a1.11 1.11 0 0 1 1.523 0c.421.403.421 1.057 0 1.46L1.838 7.44a1.11 1.11 0 0 1-1.523 0z"
+                    fill="currentColor"
+                  ></path>
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M13.685 7.44a1.11 1.11 0 0 1-1.523 0L6.238 1.762a1.002 1.002 0 0 1 0-1.46 1.11 1.11 0 0 1 1.523 0l5.924 5.678c.42.403.42 1.056 0 1.46z"
+                    fill="currentColor"
+                  ></path>
+                </svg>
+              </div>
             </div>
           </div>
+          <div class="bottom-info" v-if="!showTip">内容由 AI 生成，请仔细甄别</div>
         </div>
       </div>
     </div>
@@ -72,11 +81,18 @@
 
 <script setup lang="ts">
 import { ref } from 'vue'
+import MyQuestion from '@/components/MyQuestion.vue'
 
-const inputMessage = ref('') // 输入框的值
+const inputMessage = ref<string>('') // 输入框的值
+const messages = ref<string[]>([]) // 消息列表
+const showTip = ref<boolean>(true) // 控制 tip 的显示
+const isFixed = ref<boolean>(false) // 控制输入框是否固定在底部
 const fileInput = ref<HTMLInputElement | null>(null) // 获取文件选择框的引用
 const handleSendMessage = () => {
   console.log(inputMessage.value)
+  messages.value.push(inputMessage.value)
+  showTip.value = false
+  isFixed.value = true
   inputMessage.value = ''
 }
 
@@ -105,93 +121,129 @@ const handleUploadAttach = () => {
     color: #fff;
     display: flex;
     flex-flow: column nowrap;
-    position: relative;
     align-items: center;
 
     .header {
       height: 60px;
     }
 
-    .input-box {
+    .chat-wrapper {
+      height: calc(100% - 60px);
       width: 100%;
-      max-width: 48rem;
       display: flex;
       flex-flow: column nowrap;
       justify-content: center;
       align-items: center;
-      flex: 1 1 0%;
+      position: relative;
+      overflow: auto;
+      flex-grow: 1;
 
-      .tip {
-        margin-bottom: 1.75rem;
-        font-size: 1.875rem;
-        font-weight: 600;
-        line-height: 2.25rem;
-      }
-
-      .input-wrapper {
+      .chat-list {
         width: 100%;
-        padding: 10px;
-        border: transparent;
-        border-radius: 24px;
-        background-color: #292a2d;
+        flex: 1 1;
         display: flex;
         flex-flow: column nowrap;
-        overflow: hidden;
+        align-items: center;
 
-        .textarea-wrapper {
+        div {
           width: 100%;
-          margin: 0 4px;
-          position: relative;
+          max-width: 48rem;
+        }
+      }
 
-          textarea {
+      .input-box.fixed {
+        position: sticky;
+        bottom: 0;
+      }
+
+      .input-box {
+        width: 100%;
+        max-width: 48rem;
+        display: flex;
+        flex-flow: column nowrap;
+        justify-content: center;
+        align-items: center;
+        background-color: #212121;
+
+        .tip {
+          margin-bottom: 1.75rem;
+          font-size: 1.875rem;
+          font-weight: 600;
+          line-height: 2.25rem;
+        }
+
+        .input-wrapper {
+          box-sizing: border-box;
+          width: 100%;
+          padding: 10px;
+          border: transparent;
+          border-radius: 24px;
+          background-color: #292a2d;
+          display: flex;
+          flex-flow: column nowrap;
+          overflow: hidden;
+
+          .textarea-wrapper {
             width: 100%;
-            color: rgb(245, 245, 245);
-            line-height: 28px;
-            font-size: 16px;
-            font-family: 'Inter';
-            background-color: transparent;
-            word-break: break-word;
-            white-space: pre-wrap;
+            margin: 0 4px;
+            position: relative;
+
+            textarea {
+              width: 100%;
+              color: rgb(245, 245, 245);
+              line-height: 28px;
+              font-size: 16px;
+              font-family: 'Inter';
+              background-color: transparent;
+              word-break: break-word;
+              white-space: pre-wrap;
+            }
+          }
+
+          .button-wrapper {
+            display: flex;
+            justify-content: flex-end;
+            align-items: center;
+
+            .btn {
+              height: 32px;
+              width: 32px;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              cursor: pointer;
+            }
+
+            .attach {
+              margin-right: 10px;
+
+              svg {
+                width: 23px;
+                height: 23px;
+              }
+            }
+
+            .send.active {
+              background-color: #ccc;
+            }
+
+            .send {
+              color: #4b4b5b;
+              background-color: rgb(113 113 122);
+              border-radius: 50%;
+
+              svg {
+                width: 16px;
+                height: 16px;
+              }
+            }
           }
         }
 
-        .button-wrapper {
-          display: flex;
-          justify-content: flex-end;
-          align-items: center;
-
-          .btn {
-            height: 32px;
-            width: 32px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            cursor: pointer;
-          }
-
-          .attach {
-            margin-right: 10px;
-
-            svg {
-              width: 23px;
-              height: 23px;
-            }
-          }
-
-          .send.active {
-            background-color: #ccc;
-          }
-
-          .send {
-            color: #4b4b5b;
-            background-color: rgb(113 113 122);
-            border-radius: 50%;
-
-            svg {
-              width: 16px;
-              height: 16px;
-            }
-          }
+        .bottom-info {
+          color: #737373;
+          font-size: 12px;
+          padding: 6px 0;
         }
       }
     }
