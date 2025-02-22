481271d69a5c46eb86a3a360bb73d8a298c7edb1
shinyo-xinyi <shinyo-xinyi@users.noreply.github.com>
2025-02-22 07:43:33 +0000

    deploy: 69aae00872662c71a9926c4292c75de78383d623

---
+++ assets/InlineDialogBox-CbsFCjtz.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as c}from"./index-CuPN_VJj.js";const n={},a={class:"container"};function s(t,i){return c(),o("div",a,"this is an inline dialog box")}const r=e(n,[["render",s],["__scopeId","data-v-5dafc745"]]);export{r as default};
